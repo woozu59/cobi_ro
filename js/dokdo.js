37bb089b0f94cecdda6068cb30f5cf3c55e61f75
woozu59 <woozu5959@naver.com>
2022-05-19 17:28:28 +0900

    링크수정3차

--- js/dokdo.js
+++ js/dokdo.js
@@ -2,5 +2,19 @@
         navigation: {
           nextEl: ".swiper-button-next",
           prevEl: ".swiper-button-prev",
+        },
+        breakpoints: {
+          640: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+        },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 40,
+          },
+          1024: {
+            slidesPerView: 3,
+            spaceBetween: 50,
+          },
         }
     });
