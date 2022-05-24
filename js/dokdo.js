850533dd11091f50aac30287fcecd1012a1c5f9c
woozu59 <woozu5959@naver.com>
2022-05-24 17:25:31 +0900

    re_upload

--- js/dokdo.js
+++ js/dokdo.js
@@ -18,3 +18,16 @@
           },
         }
     });
+// $(document).ready(function(){
+//   $(window).scroll(function(){
+//     $('.title1').each(function(i){
+      
+//       var bottom_of_element = $(this).offset().top + $(this).outerHeight();
+//       var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+//       if(bottom_of_window > bottom_of_element){
+//         $(this).animate({'opacity':'1','margion-bottom':'0px'}, 1000);
+//       }
+//     });
+//   });
+// });
