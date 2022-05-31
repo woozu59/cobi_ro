adabbfca5f4089bdcada819c76d162095bd4a195
woozu59 <woozu5959@naver.com>
2022-05-31 16:04:49 +0900

    날씨 업로드

--- js/dokdo.js
+++ js/dokdo.js
@@ -2,10 +2,11 @@
 $.getJSON('https://api.openweathermap.org/data/2.5/weather?lat=37&lon=131&appid=4e406371c47d543546a019fee8fe058d&units=metric', function(result){
   let icon = `<img src="../img/weather/${result.weather[0].icon}.svg" alt="${result.weather[0].description}">`
   let temp = result.main.temp.toFixed(1)
-  $('.circle').append(icon);
+  $('.weather').append(icon);
   $('.temp').append(`${temp}℃`);
 });
-//  var weather = 
+
+
 //날씨 이미지 가져와서 날씨 이미지에 따른 날씨상황 띄울수 있도록 하고 입도희망자는 좌측메뉴 이용할수 있는 문구 띄우기
  var swiper = new Swiper(".slide", {
     navigation: {
