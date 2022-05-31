f1eb37768b1a17c3bb4174e14a6848fb849d8e87
woozu59 <woozu5959@naver.com>
2022-05-31 17:29:17 +0900

    업로드

--- js/dokdo.js
+++ js/dokdo.js
@@ -7,6 +7,17 @@ $.getJSON('https://api.openweathermap.org/data/2.5/weather?lat=37&lon=131&appid=
 });
 
 
+var sunny = $("div[src='../img/weather/01d.svg']");
+
+console.log(sunny);
+
+$(document).ready(function(){
+  if($(".weather").children().is(sunny)){
+    console.log("오늘 독도의 날씨는 맑음 입니다.");
+  } else{
+    console.log("뭔가 잘못됐다...");
+  }
+});
 //날씨 이미지 가져와서 날씨 이미지에 따른 날씨상황 띄울수 있도록 하고 입도희망자는 좌측메뉴 이용할수 있는 문구 띄우기
  var swiper = new Swiper(".slide", {
     navigation: {
