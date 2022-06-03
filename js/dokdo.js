4680b5cf0f4b3258eb11f99f01e1697aa47f61ab
woozu59 <woozu5959@naver.com>
2022-06-03 17:26:43 +0900

    기능사 실기공부

--- js/dokdo.js
+++ js/dokdo.js
@@ -1,24 +1,48 @@
-
-$.getJSON('https://api.openweathermap.org/data/2.5/weather?lat=37&lon=131&appid=4e406371c47d543546a019fee8fe058d&units=metric', function(result){
-  let icon = `<img src="../img/weather/${result.weather[0].icon}.svg" alt="${result.weather[0].description}">`
-  let temp = result.main.temp.toFixed(1)
-  $('.weather').append(icon);
-  $('.temp').append(`${temp}℃`);
-});
-
-
-var sunny = $("div[src='../img/weather/01d.svg']");
-
-console.log(sunny);
+//weather js
 
 $(document).ready(function(){
-  if($(".weather").children().is(sunny)){
-    console.log("오늘 독도의 날씨는 맑음 입니다.");
-  } else{
-    console.log("뭔가 잘못됐다...");
-  }
+      $.getJSON('https://api.openweathermap.org/data/2.5/weather?lat=37&lon=131&appid=4e406371c47d543546a019fee8fe058d&units=metric',function(result){
+          let icon = `<img src="../img/weather/${result.weather[0].icon}.svg" alt="${result.weather[0].description}">`
+          let temp = result.main.temp.toFixed(1)
+          $('.weather').append(icon);
+          $('.temp').append(`${temp}℃`);
+
+          var iconSrc = $(icon).attr('src');
+          var iconName = iconSrc.substr(iconSrc.length-7, 3);
+
+          
+          console.log(iconSrc);
+          console.log(iconName);
+          if(iconName == "01d"){
+            $(".wrap").append("<span>현재 독도의 날씨는 맑음 입니다.</span>");
+          }else if(iconName == "02d"){
+            $(".wrap").append("<span>현재 독도의 날씨는 구름 많음 입니다.</span>");
+          }else if(iconName == "03d", "04d"){
+            $(".wrap").append("<span>현재 독도의 날씨는 흐림 입니다.</span>");
+          }else if(iconName == "09d"){
+            $(".wrap").append("<span>현재 독도의 날씨는 눈 입니다.</span>");
+          }else if(iconName == "10d"){
+            $(".wrap").append("<span>현재 독도의 비 입니다.</span>");
+          }else if(iconName == "11d"){
+            $(".wrap").append("<span>현재 독도의 날씨는 번개 입니다.</span>");
+          }else if(iconName == "13d"){
+            $(".wrap").append("<span>현재 독도의 날씨는 폭설 입니다.</span>");
+          }else{
+            $(".wrap").append("<span>현재 독도의 날씨는 안개 입니다.</span>");
+          }
+        });
+
+
+//top nav js
+  $(".menu > li > ul").hide();
+  $(".menu").hover(function(){
+    $(".menu ul").stop().slideToggle();
+  });
 });
-//날씨 이미지 가져와서 날씨 이미지에 따른 날씨상황 띄울수 있도록 하고 입도희망자는 좌측메뉴 이용할수 있는 문구 띄우기
+
+
+
+//swiper 
  var swiper = new Swiper(".slide", {
     navigation: {
      nextEl: ".swiper-button-next",
