24d431d475c0158c14f71f9d3efa95d4e446f773
woozu59 <woozu5959@naver.com>
2022-06-13 17:29:39 +0900

    웹디자인 기능사 실기연습

---
+++ 웹디자인기능사/실기 공부파일/js/b-3.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    $(".gnb > li > ul").hide();
+    $(".gnb > li").hover(function(){
+        $(this).find("ul").stop().slideToggle();
+    });
+
+    setInterval(function(){
+        $(".contents1 > .imgs").delay(2000).animate({
+            marginTop: -300
+        }).delay(2000).animate({
+            marginTop: -600
+        }).delay(2000).animate({
+            marginTop: 0
+        }).delay(2000).end();
+    });
+
+    $(".notice1").click(function(){
+        $(".modalbg").show();
+    });
+    $(".bt").click(function(){
+        $(".modalbg").hide();
+    });
+});
