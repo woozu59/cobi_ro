73b0b9c8d2e38553c0a106436e4defa246f2f38a
woozu59 <woozu5959@naver.com>
2022-06-17 18:27:55 +0900

    2차 포트폴리오

--- js/dokdomb.js
+++ js/dokdomb.js
@@ -1,11 +1,17 @@
 $(document).ready(function(){
-    $(".nav > navIcon").click(function(){
-        $(".menu1").show();
+    $(".nav > .navIcon").click(function(){
+        $(".menu1").addClass("on");
     });
     $(".closeBt").click(function(){
-        $(".menu1").hide();
+        $(".menu1").removeClass("on");
     });
+    // $(".navIcon").click(function(){
+    //     $(".menu1").addClass("on");
+    // });
+   
     
+
+
     //nav 상단에 검색창 생성
     // $(".menu1 > text").html("<input type='text'>");
     // $(".menu1 > text").html("<img src='../img/icon/magnifying-glass-solid.png'>");
@@ -13,9 +19,11 @@ $(document).ready(function(){
 
 
     //좌측 아코디언 nav
-    // $(".menu > li > ul").hide();
-    // $(".menu > li").hover(function(){
-    //     $(this).find("ul").stop().slideToggle();
-    // });
+    $(".menu > li > ul").hide();
+    $(".menu > li > a").click(function(){
+        $(this).next("ul").stop().slideToggle();
+        $(".menu > li > a").not(this).next("li").slideToggle();
+        return false;
+    });
 
 });
