61b97eb93d2769d9667f9a008ac0bc9c4fba051b
woozu59 <woozu5959@naver.com>
2022-06-30 18:28:09 +0900

    lush script

---
+++ js/lush.js
@@ -0,0 +1,26 @@
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+        },
+        1200: {
+            slidePerView: 1,
+            spaceBetween: 50,
+        },
+    },
+  });
