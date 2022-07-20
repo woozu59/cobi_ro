4fd22e4d0237585993e43d8487f454736b028e88
woozu59 <woozu5959@naver.com>
2022-07-20 11:08:45 +0900

    index

--- js/index.js
+++ js/index.js
@@ -2,22 +2,11 @@ $(".vsc").draggable({containment:'parent'})
 
 $(document).ready(function(){
 
-    //프로젝트 설명란
-    $(".informs > .fileinform > .lushinform").hide();
-    $(".lushfile").click(function(){
-       $(".dokdoinform").hide();
-        $(".lushinform").show();
-    });
-
-    $(".dokdofile").click(function(){
-        $(".lushinform").hide();
-        $(".dokdoinform").show();
-    });
-    
-    
     //프로젝트 이미지
     $(".tabs > div").click(function(){
         $(this).addClass("on")
+        .siblings().removeClass("on");
+        $("." + $(this).data("id")).addClass("on")
         .siblings().removeClass("on");
         $("#" + $(this).data("id")).addClass("on")
         .siblings().removeClass("on");
