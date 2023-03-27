54c6eaff7109ec7276deb2d7a6430e5dea5826e3
woozu59 <woozu5959@naver.com>
2023-03-27 23:51:42 +0900

    연습

--- js/연습용.js
+++ js/연습용.js
@@ -1,26 +1,26 @@
-/*$(window).ready(function(){
-    draw(80, '.pie-chart1', '#ccc');
-   draw(50, '.pie-chart2', '#8b22ff');
-   draw(30, '.pie-chart3','#ff0');
- });
- 
- function draw(max, classname, colorname){
-    var i=1;
-     var func1 = setInterval(function(){
-       if(i<max){
-           color1(i,classname,colorname);
-           i++;
-       } else{
-         clearInterval(func1);
-       }
-     },10);
- }
- function color1(i, classname,colorname){
-    $(classname).css({
-         "background":"conic-gradient("+colorname+" 0% "+i+"%, #ffffff "+i+"% 100%)"
-    });
- }
- */
+ $(window).ready(function(){
+  //     draw(80, '.pie-chart1', '#ccc');
+  //    draw(50, '.pie-chart2', '#8b22ff');
+  //    draw(30, '.pie-chart3','#ff0');
+  //  });
+
+  //  function draw(max, classname, colorname){
+  //     var i=1;
+  //      var func1 = setInterval(function(){
+  //        if(i<max){
+  //            color1(i,classname,colorname);
+  //            i++;
+  //        } else{
+  //          clearInterval(func1);
+  //        }
+  //      },10);
+  //  }
+  //  function color1(i, classname,colorname){
+  //     $(classname).css({
+  //          "background":"conic-gradient("+colorname+" 0% "+i+"%, #ffffff "+i+"% 100%)"
+  //     });
+  //  }
+   // 
  
  $('.chart1').easyPieChart({
     barColor: '#f16529',
@@ -76,4 +76,14 @@
         $this.text(this.countNum);
       }
     });  
+
+
+    
+
   });
+
+
+  //----------------------html include
+
+  // $('#contents').load("/html/guidenote_1.html")
+ });  
