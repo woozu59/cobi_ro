e71fc48188d4cb5f4b0d7bd2a25638bc4c723090
woozu59 <woozu5959@naver.com>
2023-04-04 02:12:06 +0900

    dokdo

--- js/dokdomb.js
+++ js/dokdomb.js
@@ -1,22 +1,22 @@
-$(document).ready(function(){
-    $(".nav > .navIcon").click(function(){
-        $(".menu1").addClass("on");
-    });
-    $(".closeBt").click(function(){
-        $(".menu1").removeClass("on");
-    });
+// $(document).ready(function(){
+//     $(".nav > .navIcon").click(function(){
+//         $(".menu1").addClass("on");
+//     });
+//     $(".closeBt").click(function(){
+//         $(".menu1").removeClass("on");
+//     });
     
    
     
 
 
 
-    //좌측 아코디언 nav
-    $(".menu > li > ul").hide();
-    $(".menu > li").click(function(){
-        $(this).find("ul").stop().slideToggle();
-        $(".menu > li > ul").not(this).next("li").slideToggle();
-        return false;
-    });
+//     //좌측 아코디언 nav
+//     $(".menu > li > ul").hide();
+//     $(".menu > li").click(function(){
+//         $(this).find("ul").stop().slideToggle();
+//         $(".menu > li > ul").not(this).next("li").slideToggle();
+//         return false;
+//     });
 
-});
+// });
