f13c8286bb9aa8dfd407385eca0329a76f4369d0
woozu59 <woozu5959@naver.com>
2023-05-23 03:11:32 +0900

    test2

--- js/test2.js
+++ js/test2.js
@@ -1,40 +1,76 @@
-// $(function(){
-//     $('img:gt(0)').hide();
-//     setInterval(function(){
-//         $('img:first-child').fadeOut().next('img').fadeIn().end().appendTo('.slide_inner');
-//     },2000);
-// });
 $(function(){
-    var navBtn = $('.view_btn');
+  //리사이징 시 새로고침 js
+  var lastWidth = $(window).width();
+    $(window).resize(function(){
+    if($(window).width()!=lastWidth){
+      location.reload();
+      lastWidth = $(window).width();
+      return false;
+    }
+  });
+
+  //햄버거 메뉴 클릭 시 서브메뉴 펼쳐짐 js
+  var navBtn = $('.view_btn');
     navBtn.click(function(){
-        var allNav = $('li.all');
-        allNav.toggleClass('on');
+      var allNav = $('li.all');
+      allNav.toggleClass('on');
+  });
+
+  //메인 비주얼 슬라이드 fade swiper js
+  var swiper = new Swiper(".main_visual_section .mySwiper", {
+      spaceBetween: 30,
+      autoplay: {        
+          delay: 5500,
+      },
+      loop: true,
+      effect: "fade",
     });
-    
-    var swiper = new Swiper(".main_visual_section .mySwiper", {
-        spaceBetween: 30,
-        autoplay: {
-            // 자동재생 여부
-            delay: 5500, // 시작시간 설정
-        },
-        
-        loop: true,
-        effect: "fade",
 
-        pagination: {
-          el: ".swiper-pagination",
-          clickable: true,
-        },
-      });
 
-      var swiper = new Swiper("#operate_area .mySwiper", {
-        slidesPerView: 4, // 한번에 보여줄 슬라이드 개수
-        spaceBetween: 30,
-        centeredSlides: false,
-        autoplay: {
-          delay: 2500,
-          disableOnInteraction: false,
-        },
-        loop: true,
-      });
+  //운영시설 auto swiper js
+
+  var ww = $(window).width();
+    if (ww >= 641) {
+    mySwiper = new Swiper("#operate_area .mySwiper", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    centeredSlides: false,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    loop: true,
+  });
+  } else if (ww < 641) {
+  // swiper 호출 안함
+  }
+
+  //모바일 nav js
+  var navBtn = $('.mb_btn');
+    navBtn.click(function(){
+        var mbNav = $('.mb_nav');
+        mbNav.toggleClass('on');
+    });
+
+    //모바일 nav li 클릭 시 창 slide 되도록 & 방향아이콘 바뀌는 js
+    $('.gnb_mb > .depth1_mb > a').click(function(){
+      $(this).toggleClass('on').next().slideToggle(300);
+      $('.gnb_mb > .depth1_mb > a').not(this).removeClass('on').next().slideUp(300);
+      return false;
+  });
+  
+
+  //주요 공지사항 swiper js
+    var swiper = new Swiper(".mb_survey .mySwiper", {
+      slidesPerView: "auto",
+      spaceBetween: 10,
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+    });
 })
