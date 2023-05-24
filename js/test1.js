57fff386f3b4a24e2de99b6e3cf63ad3841d4074
woozu59 <woozu5959@naver.com>
2023-05-24 22:46:46 +0900

    test1

--- js/test1.js
+++ js/test1.js
@@ -25,14 +25,7 @@ $(function(){
     });
 
     //옵션리스트 클릭 시 해당 컬러장바구니영역 나타나기 js
-    //data-id 이용해서 특정 id에 addClass넣기
 
-    /*$("ul.select_list .selectbox").each(function(){
-        $(this).click(function(){
-           var check = $('#check');
-           $('li[data-id=' + $('.quanti_area').attr('id').split('')[0] + ']')
-        });
-    });*/
     var selectBox = $('li.selectbox');
     selectBox.each(function(){
         $(this).click(function(){
@@ -47,28 +40,7 @@ $(function(){
 
 
     //수량 btn 클릭 시 수량 변경하기  js
-    /*let plus = document.querySelector('.plus');
-    let minus = document.querySelector('.minus');
-    let result = document.querySelector('.price1');
-    let total = document.querySelector('.total');
-    let i = 1;
 
-    plus.addEventListener("click",() => {
-        i++
-        result.textContent = i;
-        let totalconstNum = i*17800;
-        total.textContent = totalconstNum.toLocaleString();
-    })
-    minus.addEventListener("click",() => {
-        if(i > 0){
-            i--
-            result.textContent = i;
-            let totalconstNum = i*17800;
-            total.textContent = totalconstNum.toLocaleString();
-        }else{
-            total.textContent = 0
-        }
-    })*/
 
 
     //장바구니영역의 xbtn클릭 시 삭제되기 js
@@ -78,5 +50,5 @@ $(function(){
         $('.quanti_area').css('display','none');
     });
 
-    //총 수량 및 금액 변경되기 js(증감버튼 클릭 시 동시에 적용할 수 있음)
+    //총 수량 및 금액 변경되기 js
     
